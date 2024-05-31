92328c71393a2bddcdfa248eaf25053079eae109
LucasBelemell <lucas_belemel@hotmail.com>
2024-05-31 13:06:43 -0300

    atualizacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ import { FilmesModule } from './filmes/filmes.module';
     FilmesModule,
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
-      host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT),
-      username: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DB,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true
     })
